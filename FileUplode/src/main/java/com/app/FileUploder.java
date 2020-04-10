124102352161d762548ef458a0fcbe903e0c60cc
mallesh <malle@DESKTOP-3DBORD5>
2020-04-10 23:03:13 +0530

    commitmes

---
+++ FileUplode/src/main/java/com/app/FileUploder.java
@@ -0,0 +1,40 @@
+package com.app;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+
+public class FileUploder extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletFileUpload sf=new ServletFileUpload(new DiskFileItemFactory());
+		try {
+			List<FileItem> file= sf.parseRequest(request);
+			for (FileItem element : file) {
+				element.write(new java.io.File("D:\\Core\\Hibernate\\FileUplode"+element.getName()));
+			}
+		} catch (FileUploadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
---
+++ FileUplode/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+	<h2>Hello World!</h2>
+	
+	<form action="Uploder" method="post" enctype="multipart/form-data">
+		<input type="file" name= "file" multiple>
+		<input type="submit">
+	</form>
+	
+</body>
+</html>
